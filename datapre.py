479cc22d593767603aa43aef92318a5f27ed1353
Yuhan Yang <55200109+yune-lilias@users.noreply.github.com>
2022-01-28 21:22:24 -0600

    document vectorized

---
+++ datapre.py
@@ -0,0 +1,40 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import json
+import numpy as np
+from gensim.models.doc2vec import Doc2Vec, TaggedDocument
+from nltk.tokenize import word_tokenize
+
+
+def readdata():
+    with open("bill_challenge_datasets/Training/node_features_text.json") as jf:
+        data = json.load(jf)
+
+    input = [None] * len(data)
+
+    maxint = 0
+    for k in range(len(input)):
+        input[k] = data[str(k)]
+        maxint = max(maxint, max(input[k]))
+    inputarray = np.zeros([len(input), maxint+1])
+    inputsent = []
+
+
+
+    tagged_data = [TaggedDocument(d, [i]) for i, d in enumerate(input)]
+
+    ## Train doc2vec model
+   # model = Doc2Vec(tagged_data, vector_size=200, window=2, min_count=1, workers=4, epochs=100)
+    # Save trained doc2vec model
+   # model.save("test_doc2vec.model")
+
+    ## Load saved doc2vec model
+    model = Doc2Vec.load("test_doc2vec.model")
+    ## Print model vocabulary
+    print(model.docvecs[0])
+    print(model.docvecs[1])
+    bp = 0
+
+readdata()
